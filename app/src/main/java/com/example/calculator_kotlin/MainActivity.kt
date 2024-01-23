754df5dc3a8064df892d461eaa4757b3079b4cf1
Binuki Mihara <133636115+BinukiMihara@users.noreply.github.com>
2024-01-24 00:53:42 +0530

    override OnClick implement

--- app/src/main/java/com/example/calculator_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/calculator_kotlin/MainActivity.kt
@@ -2,10 +2,51 @@ package com.example.calculator_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    lateinit var btnAdd : Button
+    lateinit var btnSubtract : Button
+    lateinit var btnMultiply : Button
+    lateinit var btnDivision : Button
+    lateinit var etA : EditText
+    lateinit var etB : EditText
+    lateinit var resultShow : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnAdd = findViewById(R.id.btn_add)
+        btnSubtract = findViewById(R.id.btn_subtraction)
+        btnMultiply = findViewById(R.id.btn_multiplication)
+        btnDivision = findViewById(R.id.btn_division)
+        etA = findViewById(R.id.et_a)
+        etB = findViewById(R.id.et_b)
+        resultShow = findViewById(R.id.result_show)
+
+    }
+
+    override fun onClick(v: View?) {
+        var a = etA.id.toString().toDouble()
+        var b = etB.id.toString().toDouble()
+        var result = 0.0 //Double value
+        when(v?.id){
+            R.id.btn_add -> {
+                result = a + b
+            }
+            R.id.btn_subtraction -> {
+                result = a - b
+            }
+            R.id.btn_multiplication -> {
+                result = a * b
+            }
+            R.id.btn_division -> {
+                result = a / b
+            }
+        }
+        resultShow.text = "Result is $result"
     }
 }
